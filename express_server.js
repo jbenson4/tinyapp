ce34dd2dc7947f2587a5de6a23225dd4eb5c5229
Jordan <jordan.benson4@gmail.com>
2022-01-24 18:48:41 -0800

    Added ability to add new tiny URL to object database

--- express_server.js
+++ express_server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = function() {
+  return Math.random().toString(36).slice(2, 8)
 };
 
 app.get("/", (req, res) => {
@@ -20,6 +26,12 @@ app.get('/urls', (req, res) => {
 
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
+});
+
+app.post("/urls", (req, res) => {
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(urlDatabase);         // Respond with 'Ok' (we will replace this)
 });
 
 app.get('/urls/:shortURL', (req, res) => {
