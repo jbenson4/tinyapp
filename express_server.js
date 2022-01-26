cce1b3479d0d24f0f5882732eef17861b60f035d
Jordan <jordan.benson4@gmail.com>
2022-01-25 18:15:37 -0800

    Added ability to delete from URL database

--- express_server.js
+++ express_server.js
@@ -14,42 +14,51 @@ const urlDatabase = {
 const generateRandomString = function() {
   return Math.random().toString(36).slice(2, 8);
 };
-
-app.get("/", (req, res) => {
+// Index Routes
+app.get('/', (req, res) => {
   res.send("Hello!");
 });
-
+// URLs Routes
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  let urlToDelete = req.params.shortURL;
+  delete urlDatabase[urlToDelete];
+  res.redirect('/urls');
+})
+
+// New URL Routes
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   let short = generateRandomString();
   urlDatabase[short] = req.body.longURL;
   console.log(req.body);
   res.redirect(`/urls/${short}`);
 });
 
+// Detailed URL Route
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
 
+// Redirect to the long URL Route
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
+app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
